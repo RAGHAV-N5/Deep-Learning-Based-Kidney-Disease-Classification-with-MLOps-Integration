4f6befc9af52d5f98a7483f5e4e054c753da27ce
Raghav <nraghav5232@gmail.com>
2024-07-06 11:04:34 +0530

    created necessary folder required

--- template.py
+++ template.py
@@ -4,7 +4,7 @@ import logging
 
 #logging string
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
-project_name = 'cnnClassifier' 
+project_name = 'KidneyDiseaseClassifier' 
 
 list_of_files= [
     ".github/workflows/.gitkeep", 
